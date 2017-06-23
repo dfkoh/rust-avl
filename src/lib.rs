0526c9ffc48c09fe1f0d4840c9857ac337b83c5b
David Koh <david.f.koh@gmail.com>
2017-06-23 01:54:43 -0400

    Why was this iterator so stupidly hard to write?

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,75 @@
+use std::rc::{Rc, Weak};
+use std::cell::RefCell;
+
 
 #[derive(Debug)]
 struct Node<T: Ord> {
-    value: T,
-    left: Option<Box<Node<T>>>,
-    right: Option<Box<Node<T>>>,
+    value: Rc<T>,
+    left: Option<NodePtr<T>>,
+    right: Option<NodePtr<T>>,
     balance: u8,
 }
 
+type NodePtr<T> = Rc<RefCell<Node<T>>>;
+
 impl<T: Ord> Node<T> {
     fn new(val: T) -> Node<T> {
-        Node { value: val, left: None, right: None, balance: 0 }
+        Node { value: Rc::new(val), left: None, right: None, balance: 0 }
+    }
+
+    fn add_child(&mut self, val: T) {
+        let child = if val < *self.value {
+            &mut self.left  
+        } else {
+            &mut self.right
+        };
+
+        match child {
+            &mut None => { *child = Some(Rc::new(RefCell::new(Node::new(val)))) },
+            &mut Some(ref node) => { node.borrow_mut().add_child(val); },
+        }
     }
 }
 
 #[derive(Debug)]
 pub struct Tree<T: Ord> {
-    root: Option<Box<Node<T>>>,
+    root: Option<NodePtr<T>>,
 }
 
 
-pub struct TreeIter<'a, T: 'a + Ord> {
-    tree: &'a Tree<T>,
-    queue: Vec<&'a T>,
+pub struct TreeIter<T: Ord> {
+    queue: Vec<Weak<RefCell<Node<T>>>>,
 }
 
-impl<'a, T: Ord> TreeIter<'a, T> {
-    fn new(tree: &'a Tree<T>) -> TreeIter<T> {
-        TreeIter { tree: &tree, queue: Vec::new() }
+impl<T: Ord> TreeIter<T> {
+    fn add_subtree(&mut self, root: Option<NodePtr<T>>) {
+        let mut node_opt = root;
+        while node_opt.is_some() {
+            let node = node_opt.unwrap();
+            self.queue.push(Rc::downgrade(&node));
+            node_opt = node.borrow().left.clone();
+        }
+    }
+
+    fn new(tree: &Tree<T>) -> TreeIter<T> {
+        let mut iter = TreeIter { queue: Vec::new() };
+        iter.add_subtree(tree.root.clone());
+        iter
     }
 }
 
-impl<'a, T: Ord> Iterator for TreeIter<'a, T> {
-    type Item = &'a T;
+impl<T: Ord> Iterator for TreeIter<T> {
+    type Item = Rc<T>;
     fn next(&mut self) -> Option<Self::Item> {
-        match self.tree.root {
+        let node_weak = self.queue.pop();
+        if node_weak.is_none() { return None; }
+
+        match node_weak.unwrap().upgrade() {
             None => None,
-            Some(ref node) => Some(&node.value),
+            Some(node) => {
+                self.add_subtree(node.borrow().right.clone());
+                Some(node.borrow().value.clone())
+            }
         }
     }
 }
@@ -46,7 +80,11 @@ impl<T: Ord> Tree<T> {
     }
     
     pub fn insert(&mut self, val: T) {
-        self.root = Some(Box::new(Node::new(val)))
+        if let Some(ref node) = self.root {
+            node.borrow_mut().add_child(val);
+        } else {
+            self.root = Some(Rc::new(RefCell::new(Node::new(val))));
+        }
     }
 
     pub fn iter(&self) -> TreeIter<T>{
@@ -77,6 +115,7 @@ mod tests {
         t.insert(123);
         t.insert(345);
         let mut itr = t.iter();
+        println!("{:?}", t);
         assert_eq!(123, *itr.next().expect("iter ended early"));
         assert_eq!(345, *itr.next().expect("iter ended early"));
     }
